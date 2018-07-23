a72b8fd0cb1e52795e6769b36657e1802e07ca42
David <xodnjs2549@naver.com>
2018-07-23 22:43:43 +0900

    setting : add gitignore, base package

---
+++ moim-manager/src/main/java/learner/moimmanager/MoimManagerApplication.java
@@ -0,0 +1,12 @@
+package learner.moimmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoimManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoimManagerApplication.class, args);
+	}
+}
