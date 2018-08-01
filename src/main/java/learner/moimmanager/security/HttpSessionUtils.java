b8121aa2b0fa73c2d0423f073b5bd12eba5285b3
David <xodnjs2549@naver.com>
2018-08-01 16:59:13 +0900

    refactor : 불필요한 라인 제거

--- src/main/java/learner/moimmanager/security/HttpSessionUtils.java
+++ src/main/java/learner/moimmanager/security/HttpSessionUtils.java
@@ -2,8 +2,6 @@ package learner.moimmanager.security;
 
 import learner.moimmanager.domain.User;
 import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.SessionScope;
 import org.springframework.web.context.request.WebRequest;
 
 import javax.servlet.http.HttpSession;
@@ -12,11 +10,11 @@ import java.util.Optional;
 public class HttpSessionUtils {
     public static String SESSION_LOGIN_USER_KEY = "loginUser";
 
-    public static boolean isLoginUser(HttpSession session) {
+    protected static boolean isLoginUser(HttpSession session) {
         return session.getAttribute(SESSION_LOGIN_USER_KEY) != null;
     }
 
-    public static Optional<User> getUserFromSession(NativeWebRequest request) {
+    protected static Optional<User> getUserFromSession(NativeWebRequest request) {
         return Optional.ofNullable(request.getAttribute(SESSION_LOGIN_USER_KEY, WebRequest.SCOPE_SESSION)).map(User.class::cast);
     }
 }
--- src/main/java/learner/moimmanager/security/LoginUserHandlerMethodArgumentResolver.java
+++ src/main/java/learner/moimmanager/security/LoginUserHandlerMethodArgumentResolver.java
@@ -1,13 +1,10 @@
 package learner.moimmanager.security;
 
-import learner.moimmanager.domain.User;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
-
-import javax.servlet.http.HttpSession;
 
 public class LoginUserHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
 
@@ -18,12 +15,6 @@ public class LoginUserHandlerMethodArgumentResolver implements HandlerMethodArgu
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-//        User user = HttpSessionUtils.getUserFromSession(webRequest);
-//        if (user != null) {
-//            return user;
-//        }
-//
-//        throw new IllegalStateException("You need login");
         return HttpSessionUtils.getUserFromSession(webRequest).orElseThrow(IllegalArgumentException::new);
     }
 }
