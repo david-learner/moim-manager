7de315e539721e16202c332f8ee49c8f3f73a861
David <xodnjs2549@naver.com>
2018-08-01 16:37:15 +0900

    refactor : HttpSessionUtilsTest SpringRunner 제거

--- src/test/java/learner/moimmanager/domain/UserTest.java
+++ src/test/java/learner/moimmanager/domain/UserTest.java
@@ -2,6 +2,7 @@ package learner.moimmanager.domain;
 
 import learner.moimmanager.dto.LoginUserDto;
 import learner.moimmanager.security.Encryption;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
@@ -13,8 +14,6 @@ import static learner.moimmanager.support.test.DummyData.DEFAULT_DB_USER;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(webEnvironment = RANDOM_PORT)
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(Encryption.class)
 public class UserTest {
@@ -26,14 +25,10 @@ public class UserTest {
 
     @Test
     public void match() {
-//        LoginUserDto loginUserDto = new LoginUserDto("hard@learner.com", "password1234");
-//        assertThat(DEFAULT_DB_USER.matches(loginUserDto), is(true));
+        PowerMockito.mockStatic(Encryption.class);
 
         LoginUserDto loginUserDto = new LoginUserDto("hard@learner.com", "password1234");
-
-        PowerMockito.mockStatic(Encryption.class);
         BDDMockito.given(Encryption.matches(loginUserDto.getPassword(), DEFAULT_DB_USER.getPassword())).willReturn(true);
-
         assertTrue(DEFAULT_DB_USER.matches(loginUserDto));
     }
 }
--- src/test/java/learner/moimmanager/security/HttpSessionUtilsTest.java
+++ src/test/java/learner/moimmanager/security/HttpSessionUtilsTest.java
@@ -2,46 +2,43 @@ package learner.moimmanager.security;
 
 import learner.moimmanager.domain.User;
 import learner.moimmanager.dto.UserDto;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.BDDMockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.mock.web.MockHttpSession;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.context.request.RequestAttributes;
 
 import javax.servlet.http.HttpSession;
-
 import java.util.Optional;
 
+import static learner.moimmanager.support.test.DummyData.DEFAULT_DB_USER;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HttpSessionUtilsTest {
     private static String SESSION_LOGIN_USER_KEY = "loginUser";
 
     @Test
     public void isLoginUser() {
-        UserDto userDto = new UserDto("황러너", "하드러너", "learner", "hard@learner.com");
-        User user = userDto.toUser();
         HttpSession httpSession = new MockHttpSession();
-        httpSession.setAttribute(SESSION_LOGIN_USER_KEY, user);
+        httpSession.setAttribute(SESSION_LOGIN_USER_KEY, DEFAULT_DB_USER);
         assertThat(HttpSessionUtils.isLoginUser(httpSession), is(true));
     }
 
     @Test
     public void getUserFromSession() {
-        UserDto userDto = new UserDto("황러너", "하드러너", "learner", "hard@learner.com");
-        User user = userDto.toUser();
         NativeWebRequest request = mock(NativeWebRequest.class);
-        request.setAttribute(SESSION_LOGIN_USER_KEY, user, RequestAttributes.SCOPE_SESSION);
+        request.setAttribute(SESSION_LOGIN_USER_KEY, DEFAULT_DB_USER, RequestAttributes.SCOPE_SESSION);
 
-        Optional<User> maybeUser = Optional.ofNullable(user);
+        Optional<User> maybeUser = Optional.ofNullable(DEFAULT_DB_USER);
         when(HttpSessionUtils.getUserFromSession(request)).thenReturn(maybeUser);
-        assertThat(maybeUser.get(),is(user));
+        assertThat(maybeUser.get().getName(), is("황러너"));
     }
 }
