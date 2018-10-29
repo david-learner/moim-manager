dba8355023ee8d081925dea0ce4843c33c4c7b34
David <xodnjs2549@naver.com>
2018-10-29 12:03:32 +0900

    refactor : group repository findAll test 추가

--- src/main/java/learner/moimmanager/repository/GroupRepository.java
+++ src/main/java/learner/moimmanager/repository/GroupRepository.java
@@ -3,5 +3,9 @@ package learner.moimmanager.repository;
 import learner.moimmanager.domain.Group;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface GroupRepository extends CrudRepository<Group, Long> {
+    @Override
+    List<Group> findAll();
 }
---
+++ src/test/java/learner/moimmanager/repository/GroupRepositoryTest.java
@@ -0,0 +1,32 @@
+package learner.moimmanager.repository;
+
+import learner.moimmanager.domain.Group;
+import learner.moimmanager.support.test.DummyData;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class GroupRepositoryTest {
+
+    @Autowired
+    private GroupRepository groupRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void findAll() {
+        userRepository.save(DummyData.DEFAULT_DB_USER);
+        groupRepository.save(DummyData.DEFAULT_GROUP);
+        assertThat(groupRepository.findAll().size(), is(1));
+    }
+}
