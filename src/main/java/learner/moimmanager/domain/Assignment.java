1fc0155004b6e63c50f4142e30369fcfbf4d2553
David <xodnjs2549@naver.com>
2019-02-25 18:58:52 +0900

    Edit assignment builder

--- src/main/java/learner/moimmanager/domain/Assignment.java
+++ src/main/java/learner/moimmanager/domain/Assignment.java
@@ -1,5 +1,7 @@
 package learner.moimmanager.domain;
 
+import org.apache.tomcat.jni.Local;
+
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,20 +20,79 @@ public class Assignment {
     private String content;
     // TINYINT 1byte -128 ~ +127
     @Column(name = "submit", columnDefinition = "TINYINT")
-    private int summitedCount;
-    private byte[] attachment;
+    private int submittedCount;
+    private Byte[] attachment;
     @Column(nullable = false)
-    private LocalDateTime start;
+    private LocalDateTime startdate;
     @Column(nullable = false)
-    private LocalDateTime end;
+    private LocalDateTime enddate;
 
     public static class Builder {
+        private Long id;
+        private Long writerId;
+        private String title;
+        private String content;
+        private int submittedCount;
+        private Byte[] attachment;
+        private LocalDateTime startdate;
+        private LocalDateTime enddate;
+
         public Builder builder() {
             return this;
         }
 
-        public Assignment build() {
-            return new Assignment();
+        public Builder id(Long val) {
+            id = val;
+            return this;
         }
+
+        public Builder writerId(Long val) {
+            writerId = val;
+            return this;
+        }
+
+        public Builder title(String val) {
+            title = val;
+            return this;
+        }
+
+        public Builder content(String val) {
+            content = val;
+            return this;
+        }
+
+        public Builder submittedCount(int val) {
+            submittedCount = val;
+            return this;
+        }
+
+        public Builder attachment(Byte[] val) {
+            attachment = val;
+            return this;
+        }
+
+        public Builder startdate(LocalDateTime val) {
+            startdate = val;
+            return this;
+        }
+
+        public Builder enddate(LocalDateTime val) {
+            enddate = val;
+            return this;
+        }
+
+        public Assignment build() {
+            return new Assignment(this);
+        }
+    }
+
+    public Assignment(Builder builder) {
+        this.writerId = builder.writerId;
+        this.title = builder.title;
+        this.content = builder.content;
+        this.submittedCount = builder.submittedCount;
+        this.attachment = builder.attachment;
+        this.startdate = builder.startdate;
+        this.enddate = builder.enddate;
     }
 }
--- src/test/java/learner/moimmanager/domain/AssignmentTest.java
+++ src/test/java/learner/moimmanager/domain/AssignmentTest.java
@@ -6,13 +6,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class AssignmentTest {
     @Test
-    public void create() {
-        Assignment assignment = new Assignment();
-    }
-
-    @Test
     public void build() {
-        Assignment assignment = new Assignment.Builder().build();
+        Assignment assignment = new Assignment.Builder().title("실행이 답이다 3주차 과제").build();
         assertThat(assignment).isNotNull();
     }
 }
